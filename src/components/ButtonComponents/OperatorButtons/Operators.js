a6fc620548427ffa8162ac741276a9efcac3492c
Chris Vasquez <chrvasq@outlook.com>
2019-09-20 22:01:50 -0700

    Setup components to display data

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,16 +1,23 @@
 import React from "react";
+import { operators } from "../../../data";
+import OperatorButton from "./OperatorButton";
 
 //import any components needed
 
 //Import your array data to from the provided data file
 
 const Operators = () => {
-  // STEP 2 - add the imported data to state
+  const [operatorState, setOperatorState] = React.useState(operators);
+
+  const buttons = operatorState.map((buttonData, index) => (
+    <OperatorButton key={index} buttonData={buttonData.value} />
+  ))
+
   return (
     <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+       {buttons}
     </div>
   );
 };
+
+export default Operators;
